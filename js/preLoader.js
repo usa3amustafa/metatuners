26dc50f135b334e08a6d3628fab9be8ffbc736e5
usa3amustafa <usa3a3ustafa@outlook.com>
2022-05-13 07:55:43 +0500

    changes

--- js/preLoader.js
+++ js/preLoader.js
@@ -1,16 +1,5 @@
 // preloader
 
-const preLoader = document.querySelector('#preloader')
-
-const preloaderImg = document.querySelector('.preloader-img')
-
-setInterval(function () {
-  preloaderImg.src = '../img/gliched-logo.gif'
-  setTimeout(function () {
-    preloaderImg.src = '../img/SPLASH PAGE/MetaTuners_M.png'
-  }, 500)
-}, 1000)
-
 window.addEventListener('load', () => {
   preLoader.style.display = 'none'
 })
